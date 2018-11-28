4f150921c9173b0568648ed5feac397e173daa60
LucasRamonF <45434420+LucasRamonF@users.noreply.github.com>
2018-11-28 16:36:20 -0200

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,18 @@ int main(){
 
     printf("SISTEMA IMOBILIÁRIO EM C"\n);
 
-
-
-
-
-
-
+    /*ADIÇÃO, EDIÇÃO REMOÇÃO DE UM IMÓVEL*/
+    
+    //CRIAR VARIÁVEL ATIVO = 0 PARA NÃO EXISTE E ATIVO = 1 PARA EXISTE IMÓVEL, OU SEJA O DELETAR SERIA ZERAR O ATIVO
+    
+    typedef struct casa{
+        char rua[10];
+        int num_quartos;
+        int ativo;
+    
+    
+    
+    }
 
 
 }
